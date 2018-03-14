68a933d8d1f98401b5f1f40abcc655eb04853002
gaomengjie <374811709@qq.com>
2018-03-14 05:28:02 -0700

    4 commit

--- C_POOL_DAY04/exc_4/my_evil_str.c
+++ C_POOL_DAY04/exc_4/my_evil_str.c
@@ -1,35 +1,35 @@
-
 //#include<stdio.h>
 
-/*int my_strlen(char *str)    
-{  
-	if(*str == '\0') 
-		return 0;   
-	else  
-	return 1+ my_strlen(str+1); 
+/*int my_strlen(const char *s)
+{
+	int len = 0;
+	while (*s++ != '\0')
+	{
+		++len;
+	}
+	return len;
 }*/
+
 char *my_evil_str(char *str)
-{  
-	int len = my_strlen(str);  
-	if(len <= 1)   
-		return 0;  
-	else  
-	{  
-		char temp = str[0];     
-        	str[0] = str[len-1];  
-        	str[len-1] = '\0';   
-        	my_evil_str(str+1); 
-        	str[len-1] = temp;  
-	}  
-}    
-  
-/*int main()  
-{  
-	char c[] = "abcdefghijklmno";
-	my_evil_str(c);  
-	printf("%s ",c);  
-	printf("\n");  
-	return 0;  
+{
+	char *start = str;
+	char *end = str + my_strlen(str)-1;
+	char temp;
+	if(str != NULL)
+	{
+		while(start<end)
+		{
+			temp = *start;
+			*start++ = *end;	
+			*end-- = temp;
+		}
+	}
+	return str;
+}
+
+/*int main()
+{
+	char c[] = "abcde";
+	printf("%s\n",c);
+	printf("%s\n",my_evil_str(c));
 }*/
-
-
